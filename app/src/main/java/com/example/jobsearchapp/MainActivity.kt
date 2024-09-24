4268573e7923fbf504430150efefe9c1463992e2
Mikhail Zabrodin <mzabrodin94@gmail.com>
2024-09-24 20:03:19 +0700

    сделал макеты

--- app/src/main/java/com/example/jobsearchapp/MainActivity.kt
+++ app/src/main/java/com/example/jobsearchapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
