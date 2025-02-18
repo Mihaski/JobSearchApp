652d6b65c653fcd1259b1335557fef45c865fe27
Mikhail Zabrodin <mzabrodin94@gmail.com>
2025-02-18 22:11:58 +0700

    доделать тестовый ресайкл, чот запарно тестить через адаптерДелага. Конкретно адаптер написать и запустить отдельно попробовать

---
+++ app/src/main/java/com/example/jobsearchapp/presentation/testlist/TestAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.jobsearchapp.presentation.testlist
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TestAdapter : RecyclerView.Adapter<TestViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/jobsearchapp/presentation/testlist/TestViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.jobsearchapp.presentation.testlist
+
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jobsearchapp.R
+
+class TestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val tvLookingNumber=view.findViewById<TextView>(R.id.count_currently_viewing)
+    val tvTitle=view.findViewById<TextView>(R.id.job_title)
+    val salary=view.findViewById<TextView>(R.id.salary)
+    val city=view.findViewById<TextView>(R.id.city)
+    val tvCompanyName=view.findViewById<TextView>(R.id.company_name)
+    val tvExpirience=view.findViewById<TextView>(R.id.experience)
+    val tvDateOfPublication=view.findViewById<TextView>(R.id.date_of_publication)
+    val isFavoriteButton=view.findViewById<ImageButton>(R.id.butt_favorites_heart_blue_or_inactive)
+}
